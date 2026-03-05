eb389b20578d7a9e28de4e972f679fdc61938093
Shivam Rathod <shivamrathod145@gmail.com>
2026-03-06 00:17:31 +0530

    Upgraded Databases conn_max

--- missingfit_backend/settings.py
+++ missingfit_backend/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = "missingfit_backend.wsgi.application"
 DATABASES = {
     "default": dj_database_url.config(
         default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
+        conn_max_age=0,
         ssl_require=True,
     )
 }
